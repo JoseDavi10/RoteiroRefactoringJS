db1dead9d60c804f208c749697437048240e6b0f
Davi Gomes <jose.davi@alu.ufc.br>
2024-11-13 22:25:51 -0300

    comit 2 - query

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 function gerarFaturaStr(fatura, pecas) {
-  const formato = new Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-    minimumFractionDigits: 2
-}).format;
-  // função extraída para calcular o total de uma apresentação
-  function calcularTotalApresentacao(apre, peca) {
+
+  // função query 
+  function getPeca(apresentacao) {
+    const formato = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+        minimumFractionDigits: 2
+    }).format;
+    let totalFatura = 0;
+    let creditos = 0;
+    let faturaStr = `Fatura ${fatura.cliente}\n`;
+
+      return pecas[apresentacao.id];
+  }
+
+  // função para calcular o total de uma apresentação
+  function calcularTotalApresentacao(apre) {
       let total = 0;
+      const peca = getPeca(apre);
       switch (peca.tipo) {
           case "tragedia":
               total = 40000;
@@ -27,22 +38,24 @@ function gerarFaturaStr(fatura, pecas) {
       return total;
   }
 
-  let totalFatura = 0;
-  let creditos = 0;
-  let faturaStr = `Fatura ${fatura.cliente}\n`;
-  
+  //ffunção para calcular os créditos de uma apresentação
+  function calcularCreditos(apre) {
+      let creditos = Math.max(apre.audiencia - 30, 0);
+      if (getPeca(apre).tipo === "comedia") creditos += Math.floor(apre.audiencia / 5);
+      return creditos;
+  }
+
+  // função para formatar a apresentação
+  function formatarApresentacao(apre) {
+      const total = calcularTotalApresentacao(apre);
+      return `  ${getPeca(apre).nome}: ${formato(total / 100)} (${apre.audiencia} assentos)\n`;
+  }
 
   for (let apre of fatura.apresentacoes) {
-      const peca = pecas[apre.id];
-      let total = calcularTotalApresentacao(apre, peca);
-      totalFatura += total;
-
-      // Cálculo dos créditos
-      creditos += Math.max(apre.audiencia - 30, 0);
-      if (peca.tipo === "comedia") creditos += Math.floor(apre.audiencia / 5);
-
-      // Adiciona a linha da apresentação na string da fatura
-      faturaStr += `  ${peca.nome}: ${formato(total / 100)} (${apre.audiencia} assentos)\n`;
+      const totalApresentacao = calcularTotalApresentacao(apre);
+      totalFatura += totalApresentacao;
+      creditos += calcularCreditos(apre);
+      faturaStr += formatarApresentacao(apre);
   }
 
   faturaStr += `Valor total: ${formato(totalFatura / 100)}\n`;
